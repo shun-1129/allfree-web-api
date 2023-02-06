4b691325e541d84b43e4a19740e5339ad9296102
Shunya Yokoi <88517400+shun-1129@users.noreply.github.com>
2023-02-07 00:02:31 +0900

    commit

---
+++ lambda/lambda_functiom.py
@@ -0,0 +1,12 @@
+def handler(event, content):
+    request_json = event['body']
+
+    print(request_json)
+
+    return {
+        'statusCode': 200,
+        'body': {
+            'content': 'Request Acceptance Complete',
+            'message': request_json
+        }
+    }
