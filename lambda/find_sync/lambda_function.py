9b86056dbf37ae647a261f7a58e4a3a2ffb2bfd5
shun-1129 <shun.1129.y@gmail.com>
2023-03-06 01:42:04 +0900

    test

--- lambda/find_sync/lambda_function.py
+++ lambda/find_sync/lambda_function.py
@@ -1,12 +1,17 @@
+import json
+from common.db_accessor import DynamoDbAccess
+
 def lambda_handler(event, content):
     request_json = event['body']
 
     print(request_json)
 
+    result = DynamoDbAccess().get_table_list_all()
+
     return {
         'statusCode': 200,
         'body': {
             'content': 'Request Acceptance Complete',
-            'message': request_json
+            'message': json.dumps(result)
         }
     }
