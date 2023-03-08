d731a57ba07ada99407c9709cc8813270f93eb95
shun-1129 <shun.1129.y@gmail.com>
2023-03-09 00:37:12 +0900

    test

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -7,12 +7,6 @@ env = os.environ
 for key, val in env.items():
     print('key:{} , val:{}'.format(key, val))
 
-try:
-    test = os.environ['branchs']
-    print(test)
-except:
-    raise Exception('Error')
-
 print('-------------------------------------------------')
 
 file_path = glob('../*')
